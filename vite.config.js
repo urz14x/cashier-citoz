0de35e0d1fc3d23cab63674b85516f19925d76ae
Ustami Rajib <ustamirazib45@gmail.com>
2025-08-05 14:38:35 +0700

    Update tailwind.config.js, vite.config.js & theme.css

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: 'https://xn40l4b6jz.sharedwithexpose.com',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/css/filament/app/theme.css', 'resources/js/app.js'],
